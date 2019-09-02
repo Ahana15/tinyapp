5a86b1622954b7d527efb1158d229acced16aba9
Ahana Ghosh <ahanaghosh94@gmail.com>
2019-09-02 18:27:20 +0000

    added random string generator function

--- express_server.js
+++ express_server.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const generateRandomString = () => {
+  let randomString = Math.random().toString(36).replace('0.', '').substring(0, 6);
+
+  return randomString;
+
+};
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
@@ -24,11 +34,21 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
